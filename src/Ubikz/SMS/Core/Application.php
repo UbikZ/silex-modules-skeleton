5d95472691762656514f413c9d6092ba09a39905
ubikz <gabrielmalet@gmail.com>
2015-05-01 23:07:17 +0200

    Change Core application and add service aliases

---
+++ src/Ubikz/SMS/Core/Application.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace Ubikz\SMS\Core;
+
+use Igorw\Silex\ConfigServiceProvider;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\SwiftmailerServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+use Ubikz\SMS\Code\Db\Configuration\ArrayConfig;
+use Ubikz\SMS\Code\Db\Connection;
+use Ubikz\SMS\Core\Exception\ErrorSQLStatementException;
+use Ubikz\SMS\Core\Exception\InvalidExtensionException;
+use Ubikz\SMS\Core\Exception\InvalidFileException;
+use Ubikz\SMS\Core\Silex\Layout as SilexLayout;
+
+/**
+ * Class Application
+ * @package Ubikz\SMS
+ */
+class Application
+{
+    /** @var bool  */
+    public $debug = false;
+
+    /**
+     * @throws InvalidConfigurationException
+     * @throws InvalidExtensionException
+     */
+    public function __construct()
+    {
+        $this->checkIntegrity();
+        $this->registerConfiguration();
+    }
+
+    /**
+     *
+     */
+    public function run()
+    {
+        try {
+            $this->registerErrorHandler();
+            $this->registerLogger();
+            $this->registerDatabase();
+            $this->registerTemplateEngine();
+
+            SilexLayout::getInstance()->run();
+        } catch (ErrorSQLStatementException $e) {
+            $this->handleException($e, 'sql');
+        } catch (\Exception $e) {
+            $this->handleException($e);
+        }
+    }
+
+    /**
+     * We'll check application integrity (extensions loaded etc.)
+     */
+    private function checkIntegrity()
+    {
+        // We need to check if pdo_mysql is enabled (no adapter for now so we have to do this)
+        if (!extension_loaded('pdo_mysql')) {
+            throw new InvalidExtensionException('Install `pdo_mysql` extension before.');
+        }
+    }
+
+    /***
+     * Turn fatal errors into exception.
+     */
+    private function registerErrorHandler()
+    {
+        ErrorHandler::register();
+        ExceptionHandler::register($this->debug);
+    }
+
+    /**
+     * @throws InvalidFileException
+     */
+    private function registerConfiguration()
+    {
+        // We get configuration file
+        if (!file_exists($confFile = CONF_PATH.'/config.yml')) {
+            throw new InvalidFileException('Configuration file `'.$confFile.'` not found.');
+        }
+
+        // We get parameters file
+        if (!file_exists($paramFile = CONF_PATH.'/parameters.yml')) {
+            throw new InvalidFileException('Parameters file `'.$paramFile.'` not found.');
+        }
+
+        SilexLayout::getInstance()->register(new ConfigServiceProvider($paramFile));
+        SilexLayout::getInstance()->register(new ConfigServiceProvider(
+            $confFile,
+            SilexLayout::getService('parameters'))
+        );
+
+        $conf = SilexLayout::getService('config.app');
+        $confApp = $conf['application'];
+
+        // Silex debugging
+        SilexLayout::setService('debug', $this->debug = isset($confApp['debug']) ? $confApp['debug'] : false);
+
+        // PHP Settings
+        if (isset($conf['php_settings']) && is_array($conf['php_settings'])) {
+            foreach ($conf['php_settings'] as $key => $value) {
+                if (false === @ini_set($key, $value)) {
+                    throw new InvalidConfigurationException('Conf `'.$key.'` => `'.$value.'` does not exist.');
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws InvalidConfigurationException
+     */
+    private function registerDatabase()
+    {
+        $conf = SilexLayout::getService('config.app');
+        if (!isset($conf['database'])) {
+            throw new InvalidConfigurationException('Configuration for `database` does not exist');
+        }
+
+        SilexLayout::setService('database', new Connection(new ArrayConfig($conf['database'])));
+    }
+
+    /**
+     * @throws InvalidConfigurationException
+     */
+    private function registerMailer()
+    {
+        $conf = SilexLayout::getService('config.app');
+        if (!isset($conf['mailer'])) {
+            throw new InvalidConfigurationException('Configuration for `mailer` does not exist');
+        }
+
+        $mailerConf = $conf['mailer'];
+        SilexLayout::getInstance()->register(new SwiftmailerServiceProvider(), [
+            'swiftmailer.options' => [
+                'host'          =>  $mailerConf['host'],
+                'port'          =>  $mailerConf['port'],
+                'username'      =>  $mailerConf['username'],
+                'password'      =>  $mailerConf['password'],
+                'encryption'    =>  $mailerConf['encryption'],
+                'auth_mode'     =>  $mailerConf['auth_mode'],
+            ],
+        ]);
+    }
+
+    /**
+     * @throws InvalidConfigurationException
+     */
+    private function registerTemplateEngine()
+    {
+        $conf = SilexLayout::getService('config.app');
+        $confApp = $conf['application'];
+        if (!isset($confApp['template'])) {
+            throw new InvalidConfigurationException('Configuration for `application.template` does not exist');
+        }
+        if (!isset($confApp['template']['path'])) {
+            throw new InvalidConfigurationException('Configuration for `application.template.path` does not exist');
+        }
+
+        // We register the default engine
+        $viewPath = ROOT_PATH.'/'.$confApp['template']['path'];
+        SilexLayout::getInstance()->register(new TwigServiceProvider(), ['twig.path' => $viewPath]);
+
+        // We define twig globals, filters, extensions etc. (if needed)
+        $app = SilexLayout::getInstance();
+        SilexLayout::setService('twig', $app->share($app->extend('twig', function ($twig, $app) {
+            /* @var $twig \Twig_Environment */
+            // Gestion de l'i18n
+            $twig->addExtension(new \Twig_Extensions_Extension_I18n());
+            // Gestion des assets
+            $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
+                // We have to use only "dist" directory to find assets
+                return sprintf('/static/dist/%s', ltrim($asset, '/'));
+            }));
+
+            return $twig;
+        })));
+
+        // We "hack" the dispatcher to "auto" redirect to defined views
+        $app->before(function ($request) use ($app, $viewPath) {
+            /* @var \Symfony\Component\HttpFoundation\Request $request */
+            if (!is_null($route = $request->attributes->get('_controller')) && is_string($route)) {
+                $regexp = '/^.+\\\(\w+)Controller::(\w+)Action$/i';
+                // Default template
+                $twigTmpl = strtolower(preg_replace($regexp, '${1}/${2}.twig', $route));
+                if (!empty($twigTmpl) && file_exists(sprintf('%s/%s', $viewPath, $twigTmpl))) {
+                    SilexLayout::setService('twig.currentTmpl', $twigTmpl);
+                }
+            }
+        });
+    }
+
+    /**
+     * todo: manage own lvl of log with own channel.
+     *
+     * @throws InvalidConfigurationException
+     */
+    public function registerLogger()
+    {
+        $conf = SilexLayout::getService('config.app');
+        $confApp = $conf['application'];
+
+        if (!isset($confApp['logger'])) {
+            throw new InvalidConfigurationException('Configuration for `application.logger` does not exist');
+        }
+        if (!isset($confApp['logger']['path'])) {
+            throw new InvalidConfigurationException('Configuration for `application.logger.path` does not exist');
+        }
+        if (!isset($confApp['logger']['channels'])) {
+            throw new InvalidConfigurationException('Configuration for `application.logger.channels` does not exist');
+        }
+
+        $channels = is_array($confApp['logger']['channels']) ? $confApp['logger']['channels'] : [];
+        $logPath = ROOT_PATH.'/'.$confApp['logger']['path'];
+
+        // Default configuration
+        SilexLayout::getInstance()->register(new MonologServiceProvider(), [
+            'monolog.logfile' => $logPath.'/'.APPLICATION_ENV.'.log',
+        ]);
+
+        // Register new channels
+        foreach ($channels as $channel) {
+            SilexLayout::setService('monolog.'.$channel, SilexLayout::getInstance()->share(
+                function ($app) use ($logPath, $channel) {
+                    /** @var Logger $log */
+                    $log = new $app['monolog.logger.class']($channel);
+                    $handler = new StreamHandler($logPath.'/'.$channel.'.log');
+                    $log->pushHandler($handler);
+
+                    return $log;
+                }
+            ));
+        }
+    }
+
+    /**
+     * @param $e \Exception
+     * @param null $channel
+     */
+    private function handleException($e, $channel = null)
+    {
+        if ($this->debug) {
+            dump($e);
+        }
+        SilexLayout::logger($channel)->addError($e->getMessage());
+    }
+}
---
+++ src/Ubikz/SMS/Core/Silex/Layout.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Ubikz\SMS\Core\Silex;
+
+use Doctrine\DBAL\Driver\PDOConnection;
+use Monolog\Logger;
+use Silex\Application;
+
+/**
+ * Class Layout
+ * @package Ubikz\SMS\Core\Silex
+ */
+class Layout
+{
+    /** @var  Application */
+    private static $instance = null;
+
+    /**
+     * @return Application
+     */
+    public static function getInstance()
+    {
+        if (null === self::$instance) {
+            self::$instance = new Application();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @param $serviceName
+     * @param bool $throw
+     * @return mixed
+     */
+    public static function getService($serviceName, $throw = true)
+    {
+        $return = null;
+        if (!isset(self::getInstance()[$serviceName])) {
+            if ($throw) {
+                throw new \InvalidArgumentException(sprintf('Service `%s` does not exist.', $serviceName));
+            }
+        } else {
+            $return = self::getInstance()[$serviceName];
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param $serviceName
+     * @param $element
+     */
+    public static function setService($serviceName, $element)
+    {
+        self::getInstance()[$serviceName] = $element;
+    }
+
+
+    /**
+     * @param $serviceName
+     */
+    public static function unsetService($serviceName)
+    {
+        if (!isset(self::getInstance()[$serviceName])) {
+            throw new \InvalidArgumentException(sprintf('Service `%s` does not exist.', $serviceName));
+        }
+
+        unset(self::getInstance()[$serviceName]);
+    }
+
+    /**
+     * @param $serviceName
+     * @return bool
+     */
+    public static function issetService($serviceName)
+    {
+        return isset(self::getInstance()[$serviceName]);
+    }
+
+    /*
+     * Service aliases
+     */
+
+    /**
+     * @return \Swift_Mailer
+     */
+    public static function mailer()
+    {
+        return self::getService('mailer');
+    }
+
+    /**
+     * @return \Twig_Environment
+     */
+    public static function template()
+    {
+        return self::getService('twig');
+    }
+
+    /**
+     * @return PDOConnection
+     */
+    public static function database()
+    {
+        return self::getService('database');
+    }
+
+    /**
+     * @param null $channel
+     *
+     * @return Logger
+     */
+    public static function logger($channel = null)
+    {
+        return self::getService('monolog'.(is_null($channel) ? '' : '.'.$channel));
+    }
+}
