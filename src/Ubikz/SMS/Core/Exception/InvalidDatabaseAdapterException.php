db07a10807825955c89e497d34de6f7cf289010d
ubikz <gabrielmalet@gmail.com>
2015-05-02 14:43:01 +0200

    Add adapter exception

---
+++ src/Ubikz/SMS/Core/Exception/InvalidDatabaseAdapterException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ubikz\SMS\Core\Exception;
+
+/**
+ * Class InvalidDatabaseAdapterException
+ * @package Ubikz\SMS\Core\Exception
+ */
+class InvalidDatabaseAdapterException extends \Exception
+{
+}
