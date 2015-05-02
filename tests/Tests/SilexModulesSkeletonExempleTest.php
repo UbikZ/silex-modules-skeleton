eaaf6708a4a9d9f9f554af1be4b0b819cf8b48b5
ubikz <gabrielmalet@gmail.com>
2015-05-02 13:54:36 +0200

    Checkstyle 2é

--- tests/Tests/SilexModulesSkeletonExempleTest.php
+++ tests/Tests/SilexModulesSkeletonExempleTest.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Tests;
 
 class SilexModulesSkeletonExempleTest extends \PHPUnit_Framework_TestCase
 {
     public function testExample()
     {
-        $testValue = "test";
-        $this->assertEquals("test", $testValue, "Error with test example");
+        $testValue = 'test';
+        $this->assertEquals('test', $testValue, 'Error with test example');
     }
 }
